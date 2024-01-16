1029d30bd36e67c6cac5e274810f908b7d0481ae
Orcadia <victorjoly40@gmail.com>
2024-01-16 16:26:43 +0100

    Testing multiple tic tac toe to verify the detection of lines

--- image_process.py
+++ image_process.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 # Charger l'image
-image = cv2.imread('test.png')
+image = cv2.imread('test3.png')
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # Appliquer un flou gaussien pour réduire le bruit
@@ -12,7 +12,7 @@ blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 edges = cv2.Canny(blurred, 50, 150)
 
 # Appliquer la Transformée de Hough pour détecter les lignes
-lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 100, minLineLength=100, maxLineGap=10)
+lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 100, minLineLength=100, maxLineGap=100)
 
 # Dessiner les lignes détectées sur l'image originale
 if lines is not None:
